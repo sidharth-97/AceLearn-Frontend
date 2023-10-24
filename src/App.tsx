fb02a2553097511004051e785de380a5b7d6468f
Sidharth PT <sidharthanand73@gmail.com>
2023-10-24 19:58:54 +0530

    Initial commit
    -Created the basic file structure for the project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+import Home from './pages/user/Home'
+
+function App() {
+
+
+  return (
+    <>
+      {/* <Home/> */}
+    
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundColor: {
+        '3447AE': '#3447AE',
+        "9ED0F5":"#9ED0F5"
+      },
+    },
+  },
+ 
+  plugins: [],
+}
